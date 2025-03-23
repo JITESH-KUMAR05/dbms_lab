943dae14e76692dcc45a96220bb98a9f6087fc99
Jitesh Kumar <150531216+JITESH-KUMAR05@users.noreply.github.com>
2025-03-23 22:12:11 +0530

    Update procedure_sal.sql

--- 23eg112c21/procedure_sal.sql
+++ 23eg112c21/procedure_sal.sql
@@ -1,30 +1,33 @@
 -- 19.	Create a procedure to update the salaries of all employees whose salary is between 25000 to 50000
-create or replace procedure sal as
-cursor cur is select eno ,ename,esal,deptno from prg19;
-a prg19.eno%type;
-b prg19.ename%type;
-c prg19.esal%type;
-d prg19.deptno%type;
-e number:=0;
-begin
-open cur;
-loop 
-fetch cur into a,b,c,d;
-if (c>25000 and c<50000) then 
-update prg19 set  esal =esal+2500;
-e:=e+1;
-fetch cur into a,b,c,d;
- dbms_output.put_line('empid is '|| a || ' empname is '|| b||'  salary is '|| c||'deptno'|| d);
-end if;
-exit when cur%notfound;
-end loop;
-close cur;
-dbms_output.put_line(' count of the salary is  :  '||  e);
-end;
+CREATE OR REPLACE PROCEDURE sal AS
+    CURSOR cur IS 
+        SELECT eno, ename, esal, deptno FROM prg19;
+    a prg19.eno%TYPE;
+    b prg19.ename%TYPE;
+    c prg19.esal%TYPE;
+    d prg19.deptno%TYPE;
+    e NUMBER := 0;
+BEGIN
+    OPEN cur;
+    LOOP 
+        FETCH cur INTO a, b, c, d;
+        EXIT WHEN cur%NOTFOUND;
+        
+        IF (c > 25000 AND c < 50000) THEN 
+            UPDATE prg19 
+            SET esal = esal + 2500 
+            WHERE eno = a;
+            e := e + 1;
+            dbms_output.put_line('empid is ' || a || ' empname is ' || b || ' salary is ' || (c + 2500) || ' deptno ' || d);
+        END IF;
+    END LOOP;
+    CLOSE cur;
+    dbms_output.put_line('Count of the updated salaries is: ' || e);
+END;
 /
 
-declare
-begin
-sal;
-end;
+DECLARE
+BEGIN
+    sal;
+END;
 /
